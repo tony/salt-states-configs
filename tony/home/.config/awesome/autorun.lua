92f58ba90dc40dc48a9088efd5039474e93b56ea
Tony Narlock <tony@git-pull.com>
2013-03-09 14:30:12 -0600

    Add ibus and nm-applet to autorun

--- tony/home/.config/awesome/autorun.lua
+++ tony/home/.config/awesome/autorun.lua
@@ -1,9 +1,9 @@
 
 --run_once("xscreensaver", "-no-splash")         -- starts screensaver daemon 
---run_once("xsetroot", "-cursor_name left_ptr")  -- sets the cursor icon
+run_once("xsetroot", "-cursor_name left_ptr")  -- sets the cursor icon
 
 --run_once("redshift", "-o -l 0:0 -b 0.5 -t 6500:6500") -- brightness
---run_once("ibus-daemon", "--xim") -- ibus
+run_once("ibus-daemon", "--xim") -- ibus
 --run_once(os.getenv("HOME") .. "/.dropbox-dist/dropboxd") -- dropbox
 run_once("nm-applet") -- networking
 --
