1f5155c200a0a997127aaf658a0de53829679f87
Tony Narlock <tony@git-pull.com>
2015-04-20 05:12:03 -0500

    Clean up, import salt_run_fabric boilerplate to run commands with salt's signature.

--- test_salt.py
+++ test_salt.py
@@ -9,16 +9,30 @@ Saltstack is a permissively-licensed, pythonic configuration management system.
 Fabric is a task-runner for all sorts of miscellaneous configurations. Both are
 essential tools.
 
-This tool will bootstrap saltstack so it can seemlessly run local python
-modules. For task management such as fabric, this should work adequately to
-fit the small gap of a tiny project, up until you need to expand to using a
-full, declarative salt configuration to manage your deployment and
-infrastructure.
-
 You can read the documentation for the commands at:
 
 - http://docs.saltstack.com/en/latest/ref/states/all/
 - http://docs.saltstack.com/en/latest/ref/modules/all/
+
+Growing big
+-----------
+
+If you think that your fabric file is getting too bit, or your needs get
+too complicated, you can migrate your project to a salt's declarative YAML
+syntax, cloud provisioning and more.
+
+You can begin to move over using a crawl-walk-run approach. First, start
+creating some initial `salt states`_ in YAML.
+
+You can start by installing to your local machine and using ``salt-ssh`` to
+setup servers. This is `Roster`_\ -based configuration.
+
+If you are beginning to manage large clusters of servers, it makes sense to
+start controlling your system via a `Salt Master`_.
+
+.. _Roster: http://docs.saltstack.com/en/latest/topics/ssh/roster.html
+.. _salt states: http://docs.saltstack.com/en/latest/topics/tutorials/starting_states.html
+.. _Salt Master: http://docs.saltstack.com/en/latest/topics/tutorials/walkthrough.html#setting-up-the-salt-master
 
 
 Copyright 2015, Tony Narlock (tony@git-pull.com). Licensed MIT.
@@ -26,8 +40,6 @@ Copyright 2015, Tony Narlock (tony@git-pull.com). Licensed MIT.
 Fabric is MIT, https://github.com/fabric/fabric/blob/master/LICENSE
 SaltStack is Apache 2: https://github.com/saltstack/salt/blob/develop/LICENSE
 """
-import os
-import logging
 
 try:
     import salt
@@ -44,9 +56,9 @@ def bootstrap_python():
     Temporary __builtins__ workaround until saltstack is refactored to handle
     importing modules pythonically.
     """
+    from salt.config import DEFAULT_MASTER_OPTS
 
     import salt.log.setup
-    from salt.config import DEFAULT_MASTER_OPTS
     salt.log.setup.setup_temp_logger(log_level='debug')
 
     __builtins__.__salt__ = {
@@ -62,9 +74,66 @@ def bootstrap_python():
         )
     )
 
-from salt.modules import git
+def salt_run_fabric(cmd,
+                    cwd=None,
+                    stdin=None,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                    output_loglevel='debug',
+                    quiet=False,
+                    runas=None,
+                    shell=DEFAULT_SHELL,
+                    python_shell=True,
+                    env=None,
+                    clean_env=False,
+                    rstrip=True,
+                    template=None,
+                    umask=None,
+                    timeout=None,
+                    with_communicate=True,
+                    reset_system_locale=True,
+                    ignore_retcode=False,
+                    saltenv='base',
+                    use_vt=False):
+
+    """Adapter to fit salt's argument signature into into fabric's
+    subprocess wrapper."""
+
+    return fabric.operations.sudo(
+
+    )
+
+
+
+def bootstrap_fabric():
+    from salt.config import DEFAULT_MASTER_OPTS
+    from salt.modules.cmdmod import DEFAULT_SHELL
+
+    try:
+        import fabric
+    except ImportError:
+        raise Exception("""
+            You must install the latest version of salt:
+            pip install fabric
+        """)
+
+    __builtins__.__salt__ = {
+        'cmd.run': salt_run_fabric,
+        'cmd.run_all': salt.modules.cmdmod.run_all
+    }
+
+    __builtins__.__grains__ = {}
+    __builtins__.__opts__ = DEFAULT_MASTER_OPTS
+    __builtins__.__salt__.update(
+        salt.loader.minion_mods(
+            __opts__
+        )
+    )
+
 
 def git_clone_salt():
+
+    from salt.modules import git
 
     git.clone(
         cwd='/home/tony/study/salt/_salt',
